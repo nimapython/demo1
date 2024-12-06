e4110213e5f3422526f1490fcea42d337523c54b
nimapython <nimanandan8@gmail.com>
2024-12-06 15:16:14 +0530

    added

---
+++ demo.py
@@ -0,0 +1,31 @@
+def demo(a):
+    c=a*a*a
+    return c
+a=int(input("Enter a number:"))
+result=demo(a)
+print("Cube=",result)
+
+
+# class calc:
+#     a=int(input("Enter the 1st no:"))
+#     b=int(input("Enter the 2nd no:"))
+#     c=input("Enter the operator:")
+    
+#     def add(x,y,z):
+#         if z
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
